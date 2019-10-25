ef6ad43d75c91781176399ad97988797275d0583
Jayden <jainovation77@gmail.com>
2019-10-25 09:05:49 +0900

    DC_Servo_modify

--- Prototype/DC_Servo_Motor_Control/JHPWMDriver/example/servoExample.cpp
+++ Prototype/DC_Servo_Motor_Control/JHPWMDriver/example/servoExample.cpp
@@ -1,14 +1,18 @@
 /*
  * The MIT License (MIT)
+
 Copyright (c) 2015 Jetsonhacks
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,8 +33,18 @@ SOFTWARE.
 // Calibrated for a Robot Geek RGS-13 Servo
 // Make sure these are appropriate for the servo being used!
 
-// int servoMin = 120 ;
-// int servoMax = 720 ;
+// int servoMin = 120 ; ref
+// int servoMax = 720 ; ref
+//60hz
+//int servoMid = 420 ;
+//int servoMin = 300 ; //left
+//int servoMax = 510 ; //right
+
+int stopDC = 307 ;
+int servoMin = 250 ; //left
+int servoMax = 430 ; //right
+int angle = 340 ;
+int speed = 307 ;
 
 int getkey() {
     int character;
@@ -63,35 +77,93 @@ int getkey() {
 int map ( int x, int in_min, int in_max, int out_min, int out_max) {
     int toReturn =  (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min ;
     // For debugging:
-    // printf("MAPPED %d to: %d\n", x, toReturn);
+    printf("MAPPED %d to: %d\n", x, toReturn);
     return toReturn ;
 }
 
 int main() {
     PCA9685 *pca9685 = new PCA9685() ;
     int err = pca9685->openPCA9685();
+    int iKeyValue = 0;
     if (err < 0){
         printf("Error: %d", pca9685->error);
     } else {
         printf("PCA9685 Device Address: 0x%02X\n",pca9685->kI2CAddress) ;
         pca9685->setAllPWM(0,0) ;
         pca9685->reset() ;
-        pca9685->setPWMFrequency(60) ;
+        pca9685->setPWMFrequency(50) ;
         // 27 is the ESC key
         printf("Hit ESC key to exit\n");
-        while(pca9685->error >= 0 && getkey() != 27){
-
-            pca9685->setPWM(0,0,servoMin) ;
-            pca9685->setPWM(1,0,servoMin) ;
-
-            sleep(2) ;
-            pca9685->setPWM(0,0,servoMax) ;
-            pca9685->setPWM(1,0,map(90,0,180,servoMin, servoMax)) ;
-            sleep(2) ;
-        }
-        pca9685->setPWM(1,0,map(0,0,180,servoMin, servoMax));
-        pca9685->setPWM(0,0,map(90,0,180,servoMin, servoMax));
         sleep(1);
+        pca9685->setPWM(1,0,307) ;
+        /*while(pca9685->error >= 0 && getkey() != 27){
+            
+            //pca9685->setPWM(0,0,map(0,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            
+            //pca9685->setPWM(0,0,map(90,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            
+            //pca9685->setPWM(0,0,map(180,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            
+            pca9685->setPWM(1,0,280) ;
+            sleep(1) ;
+            pca9685->setPWM(1,0,307) ;
+            sleep(3) ;
+        }*/
+        
+        while(pca9685->error >= 0){
+            
+            //pca9685->setPWM(0,0,map(0,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            
+            //pca9685->setPWM(0,0,map(90,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            
+            //pca9685->setPWM(0,0,map(180,0,180,servoMin, servoMax)) ;
+            //sleep(2) ;
+            iKeyValue = getkey();
+            switch(iKeyValue)
+            {
+                case 119://forward
+                        //pca9685->setPWM(1,0,280) ;
+                        speed--;
+                        pca9685->setPWM(1,0,speed) ;
+                        printf("speed up : %d\n", speed);
+                        break;
+                case 115://backward
+                        speed++;
+                        pca9685->setPWM(1,0,speed) ;
+                        printf("speed down : %d\n", speed);
+                        break;
+                case 32://break
+                        speed = stopDC;
+                        pca9685->setPWM(1,0,speed) ;
+                        printf("break value is %d\n", speed);
+                        break;
+                case 100://right
+                        if(angle <= 430){angle++;}
+                        pca9685->setPWM(0,0,angle) ;
+                        printf("Left angle is %d\n", angle);
+                        break;
+                case 97://left
+                        if(angle >= 250){angle--;}
+                        pca9685->setPWM(0,0,angle) ;
+                        printf("Left angle is %d\n", angle);
+                        break;
+                case 120:
+                        angle = map(90,0,180,servoMin, servoMax);
+                        pca9685->setPWM(0,0, angle) ;
+                        break;
+            }
+            
+            if(iKeyValue == 27)
+                break;
+        }
+        pca9685->setPWM(1,0,307);
+        pca9685->setPWM(0,0,map(90,0,180,servoMin, servoMax));
+        sleep(3);
     }
     pca9685->closePCA9685();
 }
